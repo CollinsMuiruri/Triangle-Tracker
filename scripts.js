d00c8c11b0f4d223cd84e7a6dd65bbce797678d4
Collins Muiruri <wanyekicollins@gmail.com>
2022-04-21 18:53:45 +0300

    Its Responsive! - PROJECT DONE

--- scripts.js
+++ scripts.js
@@ -7,26 +7,30 @@ function triangle() {
     let image = document.getElementById("triimage")
     let output = document.querySelector('.output')
 
-    if ((A + B > C) && (B + C > A) && (A + C > B)) {
+    if ((A == "") || (B == "") || (C == "")) {
         // window.alert("These values could form a triangle")
+        output.textContent = "INPUT ALL VALUES !";
+        image.src = "./images/load.gif"
+    } else if (A === B && B === C) {
+        output.textContent = "This is an equilateral triangle";
+        image.src = "./images/Equilateral.png"
+        console.log(A)
+    } else if ((A === B && B !== C) || (A === C && C !== B) || (B === C && C !== A)) {
+        output.textContent = "This is an isosceles triangle";
+        image.src = "./images/Isoscles.png"
+        console.log("Isoscles")
 
-        if (A === B && B === C) {
-            output.textContent = "This is an equilateral triangle";
-            image.src = "./images/Equilateral.png"
-            console.log(A)
-        } else if ((A === B && B !== C) || (A === C && C !== B) || (B === C && C !== A)) {
-            output.textContent = "This is an isosceles triangle";
-            image.src = "./images/Isoscles.png"
-            console.log("Isoscles")
-
-        } else if (A !== B && A !== C && B !== C) {
-            output.textContent = "This is an scalene triangle";
-            image.src = "./images/Scalene.png"
-            console.log("Scalene")
-
-        }
-    } else {
-        output.textContent = "These values could not form a triangle";
+    } else if (A !== B && A !== C && B !== C) {
+        output.textContent = "This is a scalene triangle";
+        image.src = "./images/Scalene.png"
+        console.log("Scalene")
 
     }
+    window.scroll({
+        top: 1000,
+        behavior: "smooth" // 👈 
+    });
+
+
+
 }
